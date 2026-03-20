0fa86f8f49d8ac4d47a08b96b204d97bf76aee61
Nash <nasiefmarhala21@gmail.com>
2026-03-20 13:14:40 +0800

    fix: adjust section title from 3xl to 4xl

--- src/components/section-title.tsx
+++ src/components/section-title.tsx
@@ -5,10 +5,10 @@ type SectionTitleProps = HeadingProps
 export default function SectionTitle(props: SectionTitleProps) {
 	return (
 		<Heading
-			size={'3xl'}
+			size={'4xl'}
 			fontWeight={'bold'}
 			textTransform={'uppercase'}
-			marginBlockEnd={{ base: '12', lg: '14' }}
+			marginBlockEnd={{ base: '12', lg: '20' }}
 			color={'secondary'}
 			{...props}
 		/>
